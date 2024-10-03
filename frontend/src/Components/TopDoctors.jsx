718407c720682a881879f62a5ad7c9cfb29dd502
sudarshan.shinde <sudarshan.shinde@inadev.com>
2024-10-03 22:05:33 +0530

    complete banner component

--- frontend/src/Components/TopDoctors.jsx
+++ frontend/src/Components/TopDoctors.jsx
@@ -12,7 +12,7 @@ const TopDoctors = () => {
         Simply browse through our extensive list of trusted doctors.
       </p>
       <div className="w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0">
-        {doctors.slice(0, 10).map((item, index) => (
+        {doctors.slice(0, 12).map((item, index) => (
           <div
             onClick={() => navigate(`/appointment/${item._id}`)}
             key={index}
