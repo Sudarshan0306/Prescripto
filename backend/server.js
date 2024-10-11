8e5c7363838e6ad174b388ce8c4c0d9576ed6fd0
sudarshan.shinde <sudarshan.shinde@inadev.com>
2024-10-11 21:06:27 +0530

    connected to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,18 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import connectDb from "./config/mongoDb.js";
 
+//app config
 const app = express();
 const port = process.env.PORT || 3000;
+connectDb()
 
 //middleware
 
 app.use(express.json());
 app.use(cors());
-    
+
 //api endpoints
 
 app.get("/", (req, res) => {
