0aa9fce450896c656c3085a33a9d023f6015b6ff
sudarshan.shinde <sudarshan.shinde@inadev.com>
2024-10-15 22:24:00 +0530

    add doctor and admin login apis created

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -2,7 +2,9 @@ import validator from "validator";
 import bcrypt from "bcrypt";
 import { v2 as cloudinary } from "cloudinary";
 import doctorModel from "../models/doctorModel.js";
-// import isEmail from "validator/lib/isEmail.js";
+import jwt from 'jsonwebtoken';
+
+
 //API for adding doctor
 
 const addDoctor = async (req, res) => {
@@ -93,7 +95,16 @@ const addDoctor = async (req, res) => {
 
 const loginAdmin = async (req, res) => {
   try {
-    
+    const { email, password } = req.body;
+    if (
+      email === process.env.ADMIN_EMAIL &&
+      password === process.env.ADMIN_PASSWORD
+    ) {
+      const token = jwt.sign(email+password, process.env.JWT_SECRET);
+      res.json({success:true, token})
+    } else {
+      res.json({ success: false, message: "Invalid Credentials" });
+    }
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: error.message });
