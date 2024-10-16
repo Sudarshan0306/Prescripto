d23ab48953b937622355c78398297a0fa71539c8
sudarshan.shinde <sudarshan.shinde@inadev.com>
2024-10-16 18:43:11 +0530

    complete admin login functionality

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -1,9 +1,49 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { assets } from "../assets/assets_admin/assets.js";
+import { AdminContext } from "../context/AdminContext.jsx";
+import { toast } from "react-toastify";
+import axios from "axios";
 const Login = () => {
   const [state, setState] = useState("Admin");
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleUserData = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    setUserData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+    try {
+      if (state === "Admin") {
+        let email = userData.email;
+        let password = userData.password;
+        const { data } = await axios.post(backendUrl + "/api/admin/login", {
+          email,
+          password,
+        });
+        if (data.success) {
+          localStorage.setItem("aToken", data.token);
+          setAToken(data.token);
+        } else {
+            toast.error(data.message)
+        }
+      } 
+    } catch (error) {}
+  };
+
+  const { setAToken, backendUrl } = useContext(AdminContext);
+
   return (
-    <form className="min-h-[80vh] flex items-center">
+    <form onSubmit={onSubmitHandler} className="min-h-[80vh] flex items-center">
       <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-[#5E5E5E] text-sm shadow-lg">
         <p className="text-2xl font-semibold m-auto">
           <span className="text-primary">{state}</span> Login
@@ -14,6 +54,9 @@ const Login = () => {
             className="border border-[#DADADA] rounded w-full p-2 mt-1"
             type="email"
             required
+            value={userData.email}
+            onChange={handleUserData}
+            name="email"
           />
         </div>
         <div className="w-full">
@@ -22,6 +65,9 @@ const Login = () => {
             className="border border-[#DADADA] rounded w-full p-2 mt-1"
             type="password"
             required
+            value={userData.password}
+            onChange={handleUserData}
+            name="password"
           />
         </div>
         <button className="bg-primary text-white w-full py-2 rounded-md text-base">
