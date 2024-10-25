b6ca203c467512b894b72b35fa92ccec25682c03
sudarshan.shinde <sudarshan.shinde@inadev.com>
2024-10-25 23:03:21 +0530

    doctosList update

--- admin/src/pages/Admin/DoctorsList.jsx
+++ admin/src/pages/Admin/DoctorsList.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { AdminContext } from '../../context/AdminContext'
 
 const DoctorsList = () => {
+  const {doctors, getAllDoctors, aToken} = useContext(AdminContext);
+
+  useEffect(()=> {
+    if (aToken) {
+      getAllDoctors()
+    }
+  }, [aToken])
   return (
-    <div>DoctorsList!</div>
+    <div>
+      
+      </div>
   )
 }
 
