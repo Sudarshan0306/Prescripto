7009d162b4ff4d01c9f2a05a90efaa3221d00ea1
sudarshan.shinde <sudarshan.shinde@inadev.com>
2024-10-27 22:43:23 +0530

    small update in doctor list page

--- admin/src/pages/Admin/DoctorsList.jsx
+++ admin/src/pages/Admin/DoctorsList.jsx
@@ -14,10 +14,19 @@ const DoctorsList = () => {
       <h1 className="text-lg font-medium">All Doctors</h1>
       <div className="w-full flex flex-wrap gap-4 pt-5 gap-y-6">
         {doctors.map((item, index) => (
-          <div key={index} className="border border-indigo-200 rounded-xl max-w-56 overflow-hidden cursor-pointer group">
-            <img className="bg-indigo-50 group-hover:bg-primary transition-all duration-300" src={item.image} alt="" />
+          <div
+            key={index}
+            className="border border-indigo-200 rounded-xl max-w-56 overflow-hidden cursor-pointer group"
+          >
+            <img
+              className="bg-indigo-50 group-hover:bg-primary transition-all duration-300"
+              src={item.image}
+              alt=""
+            />
             <div className="p-4">
-              <p className="text-neutral-800 text-lg font-medium">{item.name}</p>
+              <p className="text-neutral-800 text-lg font-medium">
+                {item.name}
+              </p>
               <p className="text-zinc-600 text-sm">{item.speciality}</p>
               <div className="mt-2 flex items-center gap-1 text-sm">
                 <input type="checkbox" checked={item.available} />
