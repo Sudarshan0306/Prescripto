eaff812689eb960d469ce6cafda7eb7c330f37f3
sudarshan.shinde <sudarshan.shinde@inadev.com>
2024-11-07 11:55:38 +0530

    added availabilty check and fetch doctors on frontend

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,16 +1,35 @@
-import { createContext } from "react";
-import { doctors } from "../assets/assets_frontend/assets";
+import { createContext, useEffect, useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 export const AppContext = createContext();
 
 const AppContextProvider = ({ children }) => {
+  const [doctors, setDoctors] = useState([]);
 
-  const currencySymbol = "$"
+  const currencySymbol = "$";
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
   const AppCtx = {
     doctors: doctors,
-    currencySymbol
+    currencySymbol,
   };
+
+  const getDoctorsData = async () => {
+    try {
+      const { data } = await axios.post(backendUrl + "/api/doctor/list");
+      if (data.success) {
+        setDoctors(data.doctors);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+  useEffect(() => {
+    getDoctorsData();
+  }, []);
 
   return <AppContext.Provider value={AppCtx}>{children}</AppContext.Provider>;
 };
